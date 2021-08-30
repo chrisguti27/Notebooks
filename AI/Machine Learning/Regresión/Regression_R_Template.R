03a0148ca6ebbc28fabd73742e0e2ce42bc02c35
chrisguti27 <57780789+chrisguti27@users.noreply.github.com>
2021-08-30 14:54:02 +0100

    Add files via upload

---
+++ AI/Machine Learning/Regresión/Regression_R_Template.R
@@ -0,0 +1,29 @@
+#Plantilla de Regresión
+
+#---------------------Importar dataset--------------
+dataset = read.csv('Position_Salaries.csv')#matriz de caracteristicas
+dataset = dataset [,2:3]
+
+
+#--------------------Ajustar modelo --------------------
+
+#*************crear variable de regresión***********
+#regression = ...
+
+#----------Predicción del modelo de regresión para un valor dado---------
+y_pred = predict(regression, newdata = data.frame(Level = 6.5))
+
+#----------------Visualización del Modelo----------------
+#install.packages("ggplot2")
+library(ggplot2)
+x_grid = seq(min(dataset$Level),max(dataset$Level,0.1))#grid para incrementar la resolución de la gráfica de la curva
+
+ggplot() +
+  geom_point(aes(x = dataset$Level,y = dataset$Salary),
+             color = "red") +
+  geom_line(aes(x = x_grid, y = predict(regression, 
+                                        newdata = data.frame(Level = x_grid))),
+            color = "blue") +
+  ggtitle("Predicción del modelo de regresión") +
+  xlab("Nivel del empleado") +
+  ylab("Sueldo (en $)")
